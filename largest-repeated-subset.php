5d095630f004577dd42739f8431b109540c3b00a
ziyed <ziyed@w3engineers.com>
2019-10-16 11:20:42 +0600

    # Improvement - Removed all testing file - Finalized LPS file

--- largest-repeated-subset.php
+++ largest-repeated-subset.php
@@ -10,78 +10,70 @@
  */
 function solve(Array $list) {    
 
-    $isDigitExist = false;
+    $longestSubstring = [];
     
     for($i = 0; $i < count($list) ; $i++){
-      if(is_string($list[$i]) && strlen($list[$i])>1){
-        array_splice($list, $i, 1);
-      }
-      if(is_numeric($list[$i])){
-        $isDigitExist = true;
-      }
-    }
+        for( $j=1+$i; $j <= count($list); $j++) {
+            $length = abs($j-$i);
 
-    if($isDigitExist){
-      for($i = 0; $i < count($list) ; $i++){
-        if(is_string($list[$i])){
-          array_splice($list, $i, 1);
+            $subStringArray = array_slice($list, $i, $length); 
+
+            $subStringLength = count($subStringArray);
+            for($k = 0; $k < count($list); $k++){
+                $checkStringArray = array_slice($list, $k, $subStringLength);
+                if(count($checkStringArray) == $subStringLength){
+                  $check = compareArray($subStringArray, $checkStringArray);
+                  if ($check && ($k > $i) ) {
+                     if( $subStringLength > count($longestSubstring)){
+                        $longestSubstring = $subStringArray;
+                     }
+                  }
+                }
+            } 
         }
-      }      
     }
-
-    $N = count($list);
-    $substrings = array();
-    
-    for ($i = 0; $i < $N; $i++){
-      $k = 0;
-      for($j=$i; $j<$N; $j++){
-        $substrings[$i][$k++] = $list[$j];
-      }      
-    }   
-
-    usort($substrings, function($a,$b) {
-        if(is_object($a[0])  || is_array($a[0])){
-            return $a > $b;  
-        }else{
-            return strcmp(implode('', $a), implode('', $b));  
-        }
-    });
-
-    $result = [];
-    for ($i = 0; $i < $N - 1; $i++){
-      $lcs = LongestCommonString($substrings[$i], $substrings[$i + 1]);
-      if (count($lcs) > count($result)){
-        $result = $lcs;
-      }
-    }
-    return $result;
+    return $longestSubstring;
 }
 
-function LongestCommonString($a, $b){
-	$n = min(count($a), count($b));
-	$result = [];
-	for ($i = 0; $i < $n; $i++){
-    if ($a[$i] === $b[$i])
-      array_push($result, $a[$i]);
-		else
-			break;
-	}
-	return $result;
+function compareArray($a, $b){
+  $return = false;
+  if(count($a) == count($b)){
+      $return = true;
+      for($i = 0; $i < count($a); $i++){
+         if($a[$i] !== $b[$i]){
+           $return = false;
+         }
+      }
+  }
+
+  return $return;
 }
 
 
 assert(solve([]) === []);
 assert(solve(['aa', 'a', 'a']) === ['a']);
 assert(solve(['b','r','o','w','n','f','o','x','h','u','n','t','e','r','n','f','o','x','r','y','h','u','n']) === ['n','f','o','x']);
-assert(solve(['b','a','n','a','n','a']) === ['a','n','a']);
-assert(function_exists('solve'));
-assert((new ReflectionFunction('solve'))->getParameters()[0]->isArray());
 assert(solve(['a', 'b', 'c', 'ab', 'c', 'a', 'b']) === ['a', 'b']);
 assert(solve([null, null, null, null, null]) === [null, null, null, null]);
 $a = new StdClass();
 assert(solve([$a, $a, $a]) === [$a, $a]);
 assert(solve([[],[], []]) === [[], []]);
+assert(function_exists('solve'));
+assert((new ReflectionFunction('solve'))->getParameters()[0]->isArray());
+assert(solve(['b','a','n','a','n','a']) === ['a','n','a']);
 assert(solve([0, 1, 0, '1', 0, 1, 0, 1]) === [0, 1, 0]);
+assert(solve([false, null, 0, false, null, '0']) === [false, null]);
+$b = clone $a;
+assert(solve([$a, $b, $a, $b]) === [$a, $b]);
+$c = function($a) {};
+$d = function($b) {};
+assert(solve([$c, $d, $c, $d]) === [$c, $d]);
+
+
+
+
+
+
 
 
 
